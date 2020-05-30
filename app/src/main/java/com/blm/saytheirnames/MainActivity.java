9ea095e23934df8d919e593f39fb1bcb863993f6
silas <slawer31@gmail.com>
2020-05-30 19:41:29 +0000

    appbar hotfixes

--- app/src/main/java/com/blm/saytheirnames/MainActivity.java
+++ app/src/main/java/com/blm/saytheirnames/MainActivity.java
@@ -1,18 +1,116 @@
 package com.blm.saytheirnames;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.blm.saytheirnames.ui.home.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String SELECTED_ITEM = "arg_selected_item";
+
+    private BottomNavigationView mBottomNav;
+    private int mSelectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+        /*getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.action_bar_layout);*/
+
+        mBottomNav = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mBottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                selectFragment(item);
+                return true;
+            }
+        });
+
+
+        MenuItem selectedItem;
+        if (savedInstanceState != null) {
+            mSelectedItem = savedInstanceState.getInt(SELECTED_ITEM, 0);
+            selectedItem = mBottomNav.getMenu().findItem(mSelectedItem);
+        } else {
+            selectedItem = mBottomNav.getMenu().getItem(0);
+        }
+        selectFragment(selectedItem);
     }
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SELECTED_ITEM, mSelectedItem);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        MenuItem homeItem = mBottomNav.getMenu().getItem(0);
+        if (mSelectedItem != homeItem.getItemId()) {
+            // select home item
+            selectFragment(homeItem);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void selectFragment(MenuItem item) {
+        Fragment frag = null;
+        // init corresponding fragment
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                frag = HomeFragment.newInstance("Home");
+                break;
+            case R.id.navigation_dashboard:
+                frag = HomeFragment.newInstance("Dash");
+                break;
+            case R.id.navigation_notifications:
+                frag = HomeFragment.newInstance("Notification");
+                break;
+        }
+
+        // update selected item
+        mSelectedItem = item.getItemId();
+
+        // uncheck the other items.
+        for (int i = 0; i < mBottomNav.getMenu().size(); i++) {
+            MenuItem menuItem = mBottomNav.getMenu().getItem(i);
+            menuItem.setChecked(menuItem.getItemId() == item.getItemId());
+        }
+
+        updateToolbarText(item.getTitle());
+
+        if (frag != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.container, frag, frag.getTag());
+            ft.commit();
+        }
+    }
+
+    private void updateToolbarText(CharSequence text) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setCustomView(R.layout.action_bar_layout);
+            actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/blm/saytheirnames/ui/home/HomeFragment.java
@@ -0,0 +1,81 @@
+package com.blm.saytheirnames.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.blm.saytheirnames.R;
+
+
+public class HomeFragment extends Fragment {
+    private static final String ARG_TEXT = "arg_text";
+    private static final String ARG_COLOR = "arg_color";
+
+    private String mText;
+    private int mColor;
+
+    private View mContent;
+    private TextView mTextView;
+
+    public static Fragment newInstance(String text) {
+        Fragment frag = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_TEXT, text);
+     //   args.putInt(ARG_COLOR, color);
+        frag.setArguments(args);
+        return frag;
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // initialize views
+        mContent = view.findViewById(R.id.navigation_dashboard);
+        mTextView = view.findViewById(R.id.text_home);
+
+        // retrieve text and color from bundle or savedInstanceState
+        /*if (savedInstanceState == null) {
+            Bundle args = getActivity().getIntent().getExtras();
+            assert args != null;
+            mText = args.getString(ARG_TEXT);
+            //mColor = args.getInt(ARG_COLOR);
+        } else {
+            mText = savedInstanceState.getString(ARG_TEXT);
+            // mColor = savedInstanceState.getInt(ARG_COLOR);
+        }*/
+
+        Bundle bundle = getActivity().getIntent().getExtras();
+        if (bundle != null)
+        {
+            mTextView.setText(" "+bundle.getString("arg_text"));
+        }
+
+        // set text and background color
+       // mTextView.setText(text);
+        //mContent.setBackgroundColor(mColor);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putString(ARG_TEXT, mText);
+       // outState.putInt(ARG_COLOR, mColor);
+        super.onSaveInstanceState(outState);
+    }
+}
